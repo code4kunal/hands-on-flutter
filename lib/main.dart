8e25d3c643416e6a2b27f4f77a6ffa5bda1afddc
Kunal Saxena <kunal1991@hotmail.com>
2020-04-29 00:59:20 +0530

    scaffolding question answer app

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,36 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  void answerQuestion(){
+    print('Answer chosen!');
+  }
   @override
   Widget build(BuildContext context) {
+    // var questions = [
+    //   'What\'s your favorite colour?',
+    //   'What\'s your favorite animal?'
+    // ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
       ),
-      body: Text('This is my default text'),
+      body: Column(children: [
+        Text('The question!'),
+        RaisedButton(
+          child: Text('Answer1'),
+          onPressed: answerQuestion,
+        ),
+        RaisedButton(
+          child: Text('Answer2'),
+          onPressed: answerQuestion,
+        ),
+        RaisedButton(
+          child: Text('Answer3'),
+          onPressed: answerQuestion,
+        ),
+      ]),
     ));
   }
 }
