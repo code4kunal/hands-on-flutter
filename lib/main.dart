4e6f5a64c6e14a485ca800a067d271a926b5402d
Kunal Saxena <kunal1991@hotmail.com>
2020-04-29 14:57:58 +0530

    added stasteless widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,38 +4,46 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  void answerQuestion(){
-    print('Answer chosen!');
+class _MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQuestion(int state) {
+    setState(() {
+      questionIndex = state;
+    });
+    print(questionIndex);
   }
+
   @override
   Widget build(BuildContext context) {
-    // var questions = [
-    //   'What\'s your favorite colour?',
-    //   'What\'s your favorite animal?'
-    // ];
+    var questions = [
+      'What\'s your favorite colour?',
+      'What\'s your favorite animal?',
+      'What\'s your favorite food?'
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(
         title: Text('My First App'),
       ),
       body: Column(children: [
-        Text('The question!'),
+        Text(questions[questionIndex]),
         RaisedButton(
           child: Text('Answer1'),
-          onPressed: answerQuestion,
+          onPressed: () => answerQuestion(0),
         ),
         RaisedButton(
           child: Text('Answer2'),
-          onPressed: () => 
-            print('Answer 2 chosen'),
+          onPressed: () => answerQuestion(1),
         ),
         RaisedButton(
           child: Text('Answer3'),
-          onPressed: () {
-            print('Answer 3 chosen');
-          },
+          onPressed: () => answerQuestion(2),
         ),
       ]),
     ));
